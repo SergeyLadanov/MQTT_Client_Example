70546701199c3fd97d20dbf851ceadaf3855e5d4
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2022-05-27 16:59:40 +0300

    add paho embedded library

--- CPP_Code_Folder/Src/file2.cpp
+++
@@ -1,6 +0,0 @@
-#include "file2.hpp"
-
-void Hello_From_CPP(void)
-{
-    printf("Hello from CPP!\r\n");
-}
--- C_Code_Folder/Src/file1.c
+++
@@ -1,8 +0,0 @@
-#include "file1.h"
-
-void Hello_From_C(void)
-{
-    printf("Hello from C!\r\n");
-}
-
-
