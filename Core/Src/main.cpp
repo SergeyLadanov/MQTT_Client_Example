d2f44f72177304c574df6f129b7c7be81c5020e1
Sergey Ladanov <sergei_ladanov_93@mail.ru>
2022-05-30 19:32:29 +0300

    add

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -4,6 +4,9 @@
 #include "MQTTPacket.h"
 
 
+
+static int globalState = 0;
+
 class MQTT_Publisher : public TCP_Client::IObserver
 {
 private:
@@ -13,8 +16,15 @@ private:
 public:
     MQTT_Publisher(char *username = nullptr, char *password = nullptr)
     {
-        sprintf(Username, username);
-        sprintf(Password, password);
+        if (username != nullptr)
+        {
+            sprintf(Username, username);
+        }
+
+        if (password != nullptr)
+        {
+            sprintf(Password, password);
+        }
     }
 private:
     void OnTcpReceived(TCP_Client *obj, uint8_t *buf, uint32_t len) override
@@ -78,28 +88,217 @@ private:
 class MQTT_Subscriber : public TCP_Client::IObserver
 {
 private:
+    typedef enum
+    {
+        STATE_NO_CON = 0,
+        STATE_CONN_ACK,
+        STATE_CONN_SUBACK
+    }ConnStates;
+
+    ConnStates State = STATE_NO_CON;
     char Username[64];
     char Password[64];
 public:
     MQTT_Subscriber(char *username = nullptr, char *password = nullptr)
     {
-        sprintf(Username, username);
-        sprintf(Password, password);
+        if (username != nullptr)
+        {
+            sprintf(Username, username);
+        }
+
+        if (password != nullptr)
+        {
+            sprintf(Password, password);
+        }
+
     }
 private:
     void OnTcpReceived(TCP_Client *obj, uint8_t *buf, uint32_t len) override
     {
         printf("Received: %s\r\n", (char *) buf);
+
+        struct ReceivedData
+        {
+            uint8_t *Buf;
+            int Offset;
+            int Len;
+        };
+
+        static ReceivedData pkt = {nullptr, 0, 0};
+        pkt.Buf = buf;
+        pkt.Offset = 0;
+        pkt.Len = len;
+
+        unsigned char local_buf[200];
+        int buflen = sizeof(local_buf);
+        int req_qos = 0;
+        int msgid = 1;
+        MQTTString topicString = MQTTString_initializer;
+
+
+
+        auto transport_getdata = [] (unsigned char* buf, int count)->int
+        {
+            int rc = 0;
+            if (count <= pkt.Len)
+            {
+                rc = count;
+            }
+            else
+            {
+                rc = pkt.Len;
+            }
+
+            memcpy(buf, &pkt.Buf[pkt.Offset], rc);
+            pkt.Offset += rc;
+
+            return rc;
+        };
+
+
+        switch (State)
+        {
+            case STATE_NO_CON :
+                /* wait for connack */
+                if (MQTTPacket_read(local_buf, buflen, transport_getdata) == CONNACK)
+                {
+                    unsigned char sessionPresent, connack_rc;
+
+                    if (MQTTDeserialize_connack(&sessionPresent, &connack_rc, local_buf, len) != 1 || connack_rc != 0)
+                    {
+                        printf("Unable to connect, return code %d\n", connack_rc);
+                        State = STATE_NO_CON;
+                    }
+                    else
+                    {
+                        printf("Success connack\n");
+                        State = STATE_CONN_ACK;
+                    }
+                }
+
+                /* subscribe */
+                topicString.cstring = (char *) "rgb_switch";
+                len = MQTTSerialize_subscribe(local_buf, buflen, 0, msgid, 1, &topicString, &req_qos);
+
+                obj->Send((uint8_t *) local_buf, len);
+
+
+            break;
+
+            case STATE_CONN_ACK :
+
+                if (MQTTPacket_read(local_buf, buflen, transport_getdata) == SUBACK) 	/* wait for suback */
+                {
+                    unsigned short submsgid;
+                    int subcount;
+                    int granted_qos;
+                    int rc = 0;
+
+                    rc = MQTTDeserialize_suback(&submsgid, 1, &subcount, &granted_qos, local_buf, buflen);
+                    if (granted_qos != 0)
+                    {
+                        printf("granted qos != 0, %d\n", granted_qos);
+                        State = STATE_NO_CON;
+                    }
+                    else
+                    {
+                        printf("Success SUB ACK\n");
+                        State = STATE_CONN_SUBACK;
+                    }
+                }
+
+                
+
+            break;
+
+            case STATE_CONN_SUBACK :
+                /* loop getting msgs on subscribed topic */
+                topicString.cstring = (char *) "rgb_state";
+
+                /* transport_getdata() has a built-in 1 second timeout,
+                your mileage will vary */
+                if (MQTTPacket_read(local_buf, buflen, transport_getdata) == PUBLISH)
+                {
+                    unsigned char dup;
+                    int qos;
+                    unsigned char retained;
+                    unsigned short msgid;
+                    int payloadlen_in;
+                    unsigned char* payload_in;
+                    int rc;
+                    MQTTString receivedTopic;
+
+                    rc = MQTTDeserialize_publish(&dup, &qos, &retained, &msgid, &receivedTopic,
+                    &payload_in, &payloadlen_in, local_buf, buflen);
+                    printf("message arrived %.*s\n", payloadlen_in, payload_in);
+
+                    if (strcmp((const char *) payload_in, "1"))
+                    {
+                        globalState = 1;
+                    }
+
+                    
+                    if (strcmp((const char *) payload_in, "0"))
+                    {
+                        globalState = 0;
+                    }
+
+
+                }
+
+                char payload[64];
+                int payloadlen = sprintf(payload, "%d", globalState);
+
+                /* Sending data */
+                printf("publishing reading\n");
+                len = MQTTSerialize_publish(local_buf, buflen, 0, 0, 0, 0, topicString, (unsigned char*)payload, payloadlen);
+                obj->Send((uint8_t *) local_buf, len);
+            break;
+
+        }
+
+
     }
 
     void OnTcpConnected(TCP_Client *obj) override
     {
         printf("Connected!\r\n");
+
+
+        MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+        int rc = 0;
+        int mysock = 0;
+        unsigned char buf[200];
+        int buflen = sizeof(buf);
+        int msgid = 1;
+
+        int req_qos = 0;
+
+        int len = 0;
+
+        data.clientID.cstring = (char *) "me1";
+        data.keepAliveInterval = 20;
+        data.cleansession = 1;
+        data.username.cstring = Username;
+        data.password.cstring = Password;
+
+        len = MQTTSerialize_connect(buf, buflen, &data);
+
+        obj->Send((uint8_t *) buf, len);
+
+        // printf("disconnecting\n");
+        // len = MQTTSerialize_disconnect(buf, buflen);
+        // rc = transport_sendPacketBuffer(mysock, buf, len);
+
+    // exit:
+    //     transport_close(mysock);
+
     }
 
     void OnTcpDisconnected(TCP_Client *obj) override
     {
         printf("Disconnected!\r\n");
+        State = STATE_NO_CON;
     }
 
     void TcpPollConnectionl(TCP_Client *obj) override
@@ -166,7 +365,7 @@ int main(int argc, char *argv[])
     while (counter)
     {
         sleep(1);
-        tcp_cl1.Connect((const char*) host, port);
+        //tcp_cl1.Connect((const char*) host, port);
         counter--;
     }
 
