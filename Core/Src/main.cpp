580419877e3c46cd29cbaa3fb564b48f6995289d
Sergey Ladanov <ladanov@digitrans.ru>
2022-05-30 14:57:15 +0300

    add temperature publishing

--- Core/Src/main.cpp
+++ Core/Src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "main.hpp"
 #include "TCP_Client.hpp"
+#include "MQTTPacket.h"
 
 
 class TCP_Observer : public TCP_Client::IObserver
@@ -14,6 +15,40 @@ private:
     void OnTcpConnected(TCP_Client *obj) override
     {
         printf("Connected!\r\n");
+
+        MQTTPacket_connectData data = MQTTPacket_connectData_initializer;
+        int rc = 0;
+        char buf[200];
+        int buflen = sizeof(buf);
+
+        MQTTString topicString = MQTTString_initializer;
+        char* payload = "20.0";
+        int payloadlen = strlen(payload);
+        int len = 0;
+
+        
+
+        data.clientID.cstring = "me";
+        data.keepAliveInterval = 20;
+        data.cleansession = 1;
+        data.username.cstring = "username";
+        data.password.cstring = "password";
+        data.MQTTVersion = 4;
+
+        len = MQTTSerialize_connect((unsigned char *)buf, buflen, &data);
+
+        topicString.cstring = "sensor";
+        len += MQTTSerialize_publish((unsigned char *)(buf + len), buflen - len, 0, 0, 0, 0, topicString, (unsigned char *)payload, payloadlen);
+
+        len += MQTTSerialize_disconnect((unsigned char *)(buf + len), buflen - len);
+
+
+        if (!obj->Send((uint8_t *)buf, len))
+            printf("Successfully published\n");
+        else
+            printf("Publish failed\n");
+
+        obj->Disconnect();
     }
 
     void OnTcpDisconnected(TCP_Client *obj) override
@@ -24,19 +59,21 @@ private:
     void TcpPollConnectionl(TCP_Client *obj) override
     {
         printf("Poll connection!\r\n");
-        obj->Send((uint8_t *)"Test", 4);
+        //obj->Send((uint8_t *)"Test", 4);
     }
 };
 
 
 TCP_Client ClTest;
 TCP_Observer Observer;
-static int counter = 10;
+static int counter = 200;
 
 // Основная программа
 int main(void)
 {
 	int err;
+    char *host = "host";
+    int port = 12608;
 
     #if defined(_WIN32) || defined(_WIN64)//Windows includes
     WSADATA wsaData;
@@ -48,17 +85,20 @@ int main(void)
 	} 
     #endif
 
-    ClTest.BindObserver(&Observer);
-    ClTest.Connect("localhost", 9090);
+    printf("Sending to hostname %s port %d\n", host, port);
 
-    while (ClTest.IsConnected())
+    ClTest.BindObserver(&Observer);
+    
+
+    while (counter)
     {
         sleep(1);
+        ClTest.Connect((const char*) host, port);
         counter--;
-        if (!counter)
-        {
-            ClTest.Disconnect();
-        }
+        // if (!counter)
+        // {
+        //     ClTest.Disconnect();
+        // }
     }
 
     sleep(2);
